f5362f7cbc3f6dd68996a8a851f4f82063d01c33
robertstoia <roberstoia@yahoo.com>
2024-01-22 16:42:56 +0200

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "../templates/views"));
@@ -79,4 +80,4 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server is up and running on port 3000"));
+app.listen(port, () => console.log(`Server is up and running on port ${port}`));
